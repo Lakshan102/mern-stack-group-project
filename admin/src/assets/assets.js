fd4b2bbdca9763adbac3c0e4be5a31b4d0fb0c2b
PramodiPeshila <ppeshila7@gmail.com>
2025-05-04 19:08:53 +0530

    created file assets.js

---
+++ admin/src/assets/assets.js
@@ -0,0 +1,21 @@
+import order_icon from './order_icon.png'
+import profile_image from './profile_image.png'
+import upload_area from './upload_area.png'
+import parcel_icon from './parcel_icon.png'
+
+
+
+import new_food from './new.png'
+
+export const assets ={
+   
+    order_icon,
+    profile_image,
+    upload_area,
+    parcel_icon,
+
+   
+    new_food
+}
+
+export const url = 'http://localhost:4000'
