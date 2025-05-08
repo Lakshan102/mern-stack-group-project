ab4422dbd5d88042580be86e1b7a3bad01d962e9
Ramesh Peshala <rameshpeshala84@gmail.com>
2025-05-08 10:47:13 +0530

    create user route file and update the file

---
+++ backend/routes/userRoute.js
@@ -0,0 +1,12 @@
+import express from "express"
+import { deleteprofile, getProfileByToken, loginUser, registerUser, updateprofile } from "../controllers/userController.js"
+
+const userRouter = express.Router()
+
+userRouter.post("/register",registerUser)
+userRouter.post("/login",loginUser)
+userRouter.post("/update",updateprofile)
+userRouter.post("/delete",deleteprofile)
+userRouter.post("/get-profile-by-token",getProfileByToken)
+
+export default userRouter;
