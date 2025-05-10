c903072295fc735c2f0df7cc293c3a66b5428300
PramodiPeshila <ppeshila7@gmail.com>
2025-05-10 22:06:33 +0530

    created IngredientsRoute file

---
+++ backend/routes/ingredientRoute.js
@@ -0,0 +1,25 @@
+import express from "express"
+import { addIngredient, listIngredient, removeIngredient, reduceQuantity, addQuantity } from "../controllers/ingredientsController.js";
+import multer from "multer"
+
+const ingredientRouter = express.Router();
+
+// image storage engine
+
+const storage = multer.diskStorage({
+    destination:"uploads",
+    filename:(req,file,cb)=>{
+        return cb(null,`${Date.now()}${file.originalname}`)
+    }
+})
+
+const upload =multer({storage:storage})
+
+ingredientRouter.post("/addIngredient",upload.single("image"),addIngredient)
+ingredientRouter.get("/listIngredient",listIngredient)
+ingredientRouter.post("/removeIngredient",removeIngredient)
+ingredientRouter.post("/reduceQuantity",reduceQuantity)
+ingredientRouter.post("/addQuantity",addQuantity)
+
+
+export default ingredientRouter;
