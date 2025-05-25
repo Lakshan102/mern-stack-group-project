64e056e5af93c1c878d226266327149b276df088
R.K.Lakshan <lakshan2725@gmail.com>
2025-05-26 00:27:16 +0530

    other assets uoload

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,33 +1,36 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Navbar from './component/navbar/navbar'
+import { Route, Routes } from 'react-router-dom'
+import Cart from './pages/cart/cart'
+import PlaceOrder from './pages/placeOrder/placeOrder'
+import Home from './pages/Home/home'
+import Footer from './component/footer/footer'
+import LoginPopup from './component/loginPopup/loginPopup'
+import Myorders from './pages/myOrders/myorder'
+import Profiles from './pages/profiles/profile'
+import Update from './pages/profiles/update'
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [showLogin,setShowLogin] = useState(false)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    {showLogin?<LoginPopup setShowLogin={setShowLogin}/>:<></>}
+      <div className='app'>
+        
+        <Navbar setShowLogin={setShowLogin} />
+
+        <Routes>
+          <Route path='/' element={<Home/>} />
+          <Route path='/cart' element={<Cart/>} />
+          <Route path='/order' element={<PlaceOrder/>} />
+          <Route path='/myorders' element={<Myorders/>}/>
+          <Route path='/profiles' element={<Profiles />} />
+          <Route path='/update' element={<Update />} />
+        </Routes>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Footer/>
     </>
   )
 }
